a87fbd01d914685c8c638e0ca9b7766be65b506b
Danna Carys <danilo.marinho13@hotmail.com>
2023-10-17 21:14:55 -0300

    Aula02-Variaveis

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,49 @@
+// var
+
+var nomeDoPrimeiroAluno = "Guillermo";
+
+var nomeDoPrimeiroAluno = "Guilhermoo";
+
+console.log(nomeDoPrimeiroAluno);
+
+// let
+
+let numeroDeAprovados = 5;
+
+numeroDeAprovados = 6;
+
+console.log(numeroDeAprovados);
+
+// clear
+
+console.clear();
+
+let nomeAluno = "Felipe";
+
+nomeAluno = "Victor";
+
+console.log(nomeAluno);
+
+// const
+
+const nomeSiteWeb = "6";
+
+console.log(nomeSiteWeb);
+
+// ------------
+
+let x = 1;
+
+let y = 22;
+
+let soma = x / y;
+
+console.log(soma);
+
+let texto1 = "game ";
+
+let texto2 = "of thrones";
+
+let somaString = texto1 + texto2;
+
+console.log(somaString);
---
+++ Tempo/script.js
@@ -0,0 +1,43 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const place = document.querySelector("#place");
+const degress = document.querySelector("#degress");
+const img = document.querySelector("img");
+const wind = document.querySelector("#wind");
+const content = document.querySelector(".content");
+
+const api_key = "264ea4e8250c55662db59bd1cfaf1022";
+
+button.addEventListener("click", () => {
+    if (!input.value) return;
+
+    getDataApi();
+})
+
+async function getDataApi() {
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+        input.value
+    )}&units=metric&appid=${api_key}`;
+
+    try {
+        await fetch(url).then((res) => res.json()).then((data) => {
+            if (data?.cod && data.cod === "404") {
+                return alert("Local não Encontrado!");
+            }
+
+            loadData(data);
+        });
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function loadData(data) {
+    console.log(data)
+    place.innerHTML = `${data.name}, ${data.sys.country}`;
+    degress.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+    img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+    content.style.display = "flex";
+}
