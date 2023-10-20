b6fd2564a21032ddaf961f12688354633b50ae02
Danna Carys <danilo.marinho13@hotmail.com>
2023-10-19 21:42:48 -0300

    Aula03-Strings

---
+++ JS/Aula03-Strings/ExercicioCasa/main.js
@@ -0,0 +1,6 @@
+const string = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log(string.charAt(19));
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.split(" ")[6]);
