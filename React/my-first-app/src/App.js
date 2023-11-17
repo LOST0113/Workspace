750d5d06b858b5ff1f9896284814194a73f7b246
Danna Carys <danilo.marinho13@hotmail.com>
2023-11-17 20:17:22 -0300

    React-3

--- React/my-first-app/src/App.js
+++ React/my-first-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Exemplo from "./components/Exemplo.jsx";
+import BoasVindas from "./components/BoasVindas";
+import Exemplo from "./components/Exemplo";
 
 function App() {
   return (
     <div className="App">
+      <BoasVindas name="Dani" />
       <Exemplo />
     </div>
   );
--- React/my-first-app/src/components/Exemplo.jsx
+++ React/my-first-app/src/components/Exemplo.jsx
@@ -1,5 +1,20 @@
-function mensagemSocorro() {
-    return <h1>Estou sofrendo para aprender React</h1>;
+import React from "react";
+
+let estilo = {color: "blue", backgroundColor: "pink", listStyle: "none"}
+
+let Exemplo = () => {
+    return (
+        <>
+            <h1>Sofrendo nessa sexta</h1>
+            <h2>Reprovam</h2>
+            <ul style={estilo}>
+                <li>Aluno 1</li>
+                <li>Aluno 2</li>
+                <li>Aluno 3</li>
+                <li>Todos alunos foram reprovados!</li>
+            </ul>
+        </>
+    );
 }
 
-export default mensagemSocorro;
+export default Exemplo;
